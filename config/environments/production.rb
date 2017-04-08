42ce10c0cab44d83df7509598fb54205c19f8758
dpmango <xs290@me.com>
2017-04-08 20:45:39 +0300

    gmail mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,16 @@ Rails.application.configure do
 
   config.action_cable.url = "wss://winsouq.herokuapp.com/cable"
 
+  config.action_mailer.default_url_options = {:host => 'winsouq.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['gmail_username'],
+    :password             => ENV['gmail_password'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
 end
