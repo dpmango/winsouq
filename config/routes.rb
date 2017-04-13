525a5ea664bf8976f0996a7f34bbcafef89229fe
dpmango <xs290@me.com>
2017-04-13 19:38:22 +0300

    rescue errors on active_admin start

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
